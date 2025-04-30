6b847e00d39f9b8e2481fc8d60226c7d9f53188d
Josquin Harrison <josquin.harrison@inria.fr>
2025-04-30 19:54:02 +0200

    Added tests

--- playground/curves/fourier.py
+++ playground/curves/fourier.py
@@ -5,6 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def mesh_curve(curve):
+    zer = np.zeros((len(curve), 1))
+    curve_points = np.append(curve, zer, axis=1)
+    l1 = np.arange(0, len(curve_points))
+    lines = np.concatenate((np.array([len(curve_points)]), l1))
+    curve = pv.PolyData(curve_points, lines)
+    mesh = curve.triangulate()
+    return mesh
+
+
 def triangulate_curve(curve):
     zer = np.zeros((len(curve), 1))
     curve_points = np.append(curve, zer, axis=1)
@@ -21,35 +31,26 @@ def triangulate_curve(curve):
 
 def fourier_features(triangles, k_range):
     ks = np.mgrid[0 : k_range + 0.1 : 1, -k_range : k_range + 0.1 : 1].reshape(2, -1).T
-    # Get M and det(M)
+
     m = triangles[:, :2] - triangles[:, 2][:, np.newaxis]
     mt = m.transpose(0, 2, 1)
-
-    # Get the (M.t*k)
-    mtks = np.matvec(mt, ks[:, np.newaxis, :]).transpose(
-        1, 0, 2
-    )  # TODO: Check if this is correct
-
-    # Get the base fourier coeffs for the other triangles
-    a = np.power(mtks[:, :, 1] * (mtks[:, :, 1] - mtks[:, :, 0]), -1)
-    b = np.power(mtks[:, :, 0] * mtks[:, :, 1], -1)
-    sTks = a * np.sin(mtks[:, :, 0]) + (b - a) * np.sin(mtks[:, :, 1])
-    cTks = a * np.cos(mtks[:, :, 1]) + (a + b) * np.cos(mtks[:, :, 0]) - b
-    # TODO: Figure out what to do with the NANS
-
-    # Get the fourier coeffs for each triangles
     det_m = np.linalg.det(m)
+
+    mtks = np.matvec(mt, ks[:, np.newaxis, :]).transpose(1, 0, 2)
+    a = np.power(mtks[:, :, 1] * (mtks[:, :, 0] - mtks[:, :, 1]), -1)
+    b = np.power(mtks[:, :, 0] * mtks[:, :, 1], -1)
+    sin_k1, sin_k2 = np.sin(mtks[:, :, 0]), np.sin(mtks[:, :, 1])
+    cos_k1, cos_k2 = np.cos(mtks[:, :, 0]), np.cos(mtks[:, :, 1])
+    sTks = a * (sin_k2 - sin_k1) + b * sin_k1
+    cTks = a * (cos_k2 - cos_k1) + b * (cos_k1 - 1)
+
     cos = np.cos(np.inner(ks, triangles[:, 2])).T
     sin = np.sin(np.inner(ks, triangles[:, 2])).T
     stk = det_m[:, np.newaxis] * (sTks * cos + cTks * sin)
     ctk = det_m[:, np.newaxis] * (cTks * cos - sTks * sin)
-    # TODO: check if inner is the correct function to use here
 
-    # Sum and stack to get the shape fourier features
     triangles_features = np.stack((stk, ctk), axis=-1)
     shape_features = np.sum(triangles_features, axis=0)
-    # Get rid of the nans here:
-    shape_features = shape_features[~np.isnan(shape_features)].reshape((-1, 2))
 
     return shape_features
 
@@ -60,7 +61,7 @@ def main():
     curve_0 = curves[0] / 1.5
     curve_1 = curves[1] / 1.5
 
-    k_range = 200
+    k_range = 100
 
     triangles = triangulate_curve(curve_0)
     fourier_feats_0 = fourier_features(triangles, k_range)
@@ -68,13 +69,31 @@ def main():
     triangles = triangulate_curve(curve_1)
     fourier_feats_1 = fourier_features(triangles, k_range)
 
-    plt.title(f"The first {len(fourier_feats_0)} fourier features")
+    curve_1_mesh = mesh_curve(curve_0)
+    curve_1_mesh.plot(show_edges=True)
+
+    plt.title(f"The first {len(fourier_feats_0)} features for 2 different curves")
     plt.plot(fourier_feats_0[:, 0], label="S_k1")
     plt.plot(fourier_feats_0[:, 1], label="C_k1")
     plt.plot(fourier_feats_1[:, 0], label="S_k2")
     plt.plot(fourier_feats_1[:, 1], label="C_k2")
     plt.legend()
     plt.show()
+
+    Sks = fourier_feats_0[:, 0]
+    Cks = fourier_feats_0[:, 1]
+    Sks_xy = Sks[~np.isnan(Sks)].reshape(202, 100)
+    Cks_xy = Cks[~np.isnan(Cks)].reshape(202, 100)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle("Coefs wrt k_range of first curve", fontsize=20)
+    ax1.set_title("Sks coefs")
+    ax1.imshow(Sks_xy)
+    ax2.set_title("Cks coefs")
+    ax2.imshow(Cks_xy)
+    plt.show()
+
+    breakpoint()
 
 
 if __name__ == "__main__":
